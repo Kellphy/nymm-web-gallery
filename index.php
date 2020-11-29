d69313e8f1754429836b3c0204285d383da9a7ba
Kellphy <virtual_44@yahoo.com>
2020-11-29 18:49:16 +0200

    updated

--- index.php
+++ index.php
@@ -231,12 +231,11 @@
 
     -->
 
-
     <script src="/assets/plugins/slick-1.8.0/slick/slick.min.js"></script>
     <script src="/assets/plugins/flex-menu/flexmenu.min.js"></script>
     <script src="/assets/plugins/jquery-match-height-master/dist/jquery.matchHeight.js"></script>
     <script src="/assets/plugins/muuri-master/muuri.js"></script>
-    <script src="/assets/plugins/lightbox2-master/dist/js/lightbox.js"></script>
+
     <script src="/assets/plugins/wow-master/dist/wow.min.js"></script>
     <script src="/assets/js/script.js"></script>
 
