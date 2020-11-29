2d423a7d92892f3ee86b4482349c2d3bd8e705f8
Kellphy <virtual_44@yahoo.com>
2020-11-29 18:36:31 +0200

    updated

--- index.php
+++ index.php
@@ -71,114 +71,7 @@
             }
         }
     </style>
-    <script type="text/javascript">
-        window.N2PRO = 1;
-        window.N2GSAP = 1;
-        window.N2PLATFORM = "wordpress";
-        (function () {
-            var N = this;
-            N.N2_ = N.N2_ || {
-                r: [],
-                d: []
-            }, N.N2R = N.N2R || function () {
-                N.N2_.r.push(arguments)
-            }, N.N2D = N.N2D || function () {
-                N.N2_.d.push(arguments)
-            }
-        }).call(window);
-        if (!window.n2jQuery) {
-            window.n2jQuery = {
-                ready: function (cb) {
-                    console.error('n2jQuery will be deprecated!');
-                    N2R(['$'], cb);
-                }
-            }
-        }
-        window.nextend = {
-            localization: {},
-            ready: function (cb) {
-                console.error('nextend.ready will be deprecated!');
-                N2R('documentReady', function ($) {
-                    cb.call(window, $)
-                })
-            }
-        };
-        window.N2SSPRO = 1;
-        window.N2SS3C = "smartslider3";
-        nextend.fontsLoaded = false;
-        nextend.fontsLoadedActive = function () {
-            nextend.fontsLoaded = true;
-        };
-        var fontData = {
-            google: {
-                families: ["Baloo:300,400:latin"]
-            },
-            active: function () {
-                nextend.fontsLoadedActive()
-            },
-            inactive: function () {
-                nextend.fontsLoadedActive()
-            }
-        };
-        if (typeof WebFontConfig !== 'undefined') {
-            var _WebFontConfig = WebFontConfig;
-            for (var k in WebFontConfig) {
-                if (k == 'active') {
-                    fontData.active = function () {
-                        nextend.fontsLoadedActive();
-                        _WebFontConfig.active();
-                    }
-                } else if (k == 'inactive') {
-                    fontData.inactive = function () {
-                        nextend.fontsLoadedActive();
-                        _WebFontConfig.inactive();
-                    }
-                } else if (k == 'google') {
-                    if (typeof WebFontConfig.google.families !== 'undefined') {
-                        for (var i = 0; i < WebFontConfig.google.families.length; i++) {
-                            fontData.google.families.push(WebFontConfig.google.families[i]);
-                        }
-                    }
-                } else {
-                    fontData[k] = WebFontConfig[k];
-                }
-            }
-        }
-        if (typeof WebFont === 'undefined') {
-            window.WebFontConfig = fontData;
-        } else {
-            WebFont.load(fontData);
-        }
-    </script>
-    <script type="text/javascript" src="/assets/plugins/slider/js/n2-j.min.js"></script>
-    <script type="text/javascript" src="/assets/plugins/slider/js/nextend-gsap.min.js"></script>
-    <script type="text/javascript" src="/assets/plugins/slider/js/nextend-frontend.min.js"></script>
-    <script type="text/javascript" src="/assets/plugins/slider/js/smartslider-frontend.min.js"></script>
-    <script type="text/javascript" src="/assets/plugins/slider/js/smartslider-simple-type-frontend.min.js"></script>
-    <script type="text/javascript" src="/assets/plugins/slider/js/nextend-webfontloader.min.js"></script>
-    <script type="text/javascript">
-        N2R('documentReady', function ($) {
 
-            nextend.fontsDeferred = $.Deferred();
-            if (nextend.fontsLoaded) {
-                nextend.fontsDeferred.resolve();
-            } else {
-                nextend.fontsLoadedActive = function () {
-                    nextend.fontsLoaded = true;
-                    nextend.fontsDeferred.resolve();
-                };
-                var intercalCounter = 0;
-                nextend.fontInterval = setInterval(function () {
-                    if (intercalCounter > 3 || document.documentElement.className.indexOf('wf-active') !==
-                        -1) {
-                        nextend.fontsLoadedActive();
-                        clearInterval(nextend.fontInterval);
-                    }
-                    intercalCounter++;
-                }, 1000);
-            }
-        });
-    </script>
 <!-- <script src="https://kit.Baloo.com/feab4e393b.js"></script> -->
 
 <style>
@@ -240,14 +133,14 @@ body, html {
     <!-- Scripts 
 
     
-    <script src="/assets/plugins/jquery-match-height-master/dist/jquery.matchHeight.js"></script>
+    
 
     -->
 
 
     <script src="/assets/plugins/slick-1.8.0/slick/slick.min.js"></script>
     <script src="/assets/plugins/flex-menu/flexmenu.min.js"></script>
-
+    <script src="/assets/plugins/jquery-match-height-master/dist/jquery.matchHeight.js"></script>
     <script src="/assets/plugins/muuri-master/muuri.js"></script>
     <script src="/assets/plugins/lightbox2-master/dist/js/lightbox.js"></script>
     <script src="/assets/plugins/wow-master/dist/wow.min.js"></script>
