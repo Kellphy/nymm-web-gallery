e5ca82e820e13c570f8bbdda90d51903d00b0cd7
Kellphy <virtual_44@yahoo.com>
2021-03-23 23:46:04 +0200

    23

--- assets/footer.php
+++ assets/footer.php
@@ -5,10 +5,10 @@
             <div class="row">
                 <div class="copyright ptb30 col-lg-12">
                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                        © Kellphy 2020 [kellphy.com]
+                        © Kellphy 2020-2021 - All Rights Reserved 
                     </div>
                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-right">
-                        All Rights Reserved
+                        <a href="https://kellphy.com/">kellphy.com</a>
                     </div>
                 </div>
             </div>
